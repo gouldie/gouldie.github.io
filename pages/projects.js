bab1353e2011c31af5c28ec558fe272f60519b11
matthew.gould <matthew.gould@morneaushepell.com>
2023-02-01 19:12:18 +0000

    add collector project

--- pages/projects.js
+++ pages/projects.js
@@ -23,7 +23,17 @@ const projects = [
       'A clone of reddit in the style of Vuetify. Create, read, update, and delete posts. Vote and comment.',
     tech: ['VueJS', 'SASS', 'NodeJS', 'Express', 'MongoDB', 'JWT'],
     url: 'https://redditmg.herokuapp.com'
-  }
+  },
+  {
+    name: 'Collector',
+    image: 'collector.png',
+    description:
+      'Pokemon trading card collection tracker. Persists to local storage. Light/dark modes.',
+    tech: ['React', 'Next.js'],
+    url: 'https://collector-three.vercel.app'
+  },
+  { name: 'empty-1', empty: true },
+  { name: 'empty-2', empty: true }
 ]
 
 export default function Projects() {
@@ -38,59 +48,63 @@ export default function Projects() {
       </p>
 
       <div className='flex justify-center flex-wrap'>
-        {projects.map(project => (
-          <div
-            key={project.name}
-            className='flex flex-col max-w-xs bg-white m-5 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 border-gray-400 dark:border-gray-700'
-          >
-            <a href={project.url} target='_blank' rel='noreferrer noopener'>
-              <img className='rounded-t-lg' src={project.image} alt={`go to ${project.name}`} />
-            </a>
-            <div className='p-5 flex flex-col justify-between items-start grow'>
-              <div>
-                <a href={project.url} target='_blank' rel='noreferrer noopener'>
-                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                    {project.name}
-                  </h5>
-                </a>
-                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
-                  {project.description}
-                </p>
-
-                <div className='flex flex-wrap mb-2'>
-                  {project.tech.map(t => (
-                    <span
-                      key={t}
-                      className='bg-gray-300 text-gray-800 text-sm font-medium mr-2 mb-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300'
-                    >
-                      {t}
-                    </span>
-                  ))}
-                </div>
-              </div>
-              <a
-                href={project.url}
-                rel='noreferrer noopener'
-                target='_blank'
-                className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-              >
-                Go to site
-                <svg
-                  className='ml-2 -mr-1 w-4 h-4'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-                    clipRule='evenodd'
-                  ></path>
-                </svg>
+        {projects.map(project =>
+          project.empty ? (
+            <div key={project.name} className='max-w-xs m-5 w-full' />
+          ) : (
+            <div
+              key={project.name}
+              className='flex flex-col max-w-xs bg-white m-5 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 border-gray-400 dark:border-gray-700'
+            >
+              <a href={project.url} target='_blank' rel='noreferrer noopener'>
+                <img className='rounded-t-lg' src={project.image} alt={`go to ${project.name}`} />
               </a>
+              <div className='p-5 flex flex-col justify-between items-start grow'>
+                <div>
+                  <a href={project.url} target='_blank' rel='noreferrer noopener'>
+                    <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                      {project.name}
+                    </h5>
+                  </a>
+                  <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+                    {project.description}
+                  </p>
+
+                  <div className='flex flex-wrap mb-2'>
+                    {project.tech.map(t => (
+                      <span
+                        key={t}
+                        className='bg-gray-300 text-gray-800 text-sm font-medium mr-2 mb-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300'
+                      >
+                        {t}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <a
+                  href={project.url}
+                  rel='noreferrer noopener'
+                  target='_blank'
+                  className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                >
+                  Go to site
+                  <svg
+                    className='ml-2 -mr-1 w-4 h-4'
+                    fill='currentColor'
+                    viewBox='0 0 20 20'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                      clipRule='evenodd'
+                    ></path>
+                  </svg>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </Layout>
   )
