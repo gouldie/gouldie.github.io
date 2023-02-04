8e8e4fa8440debfb0d158519d4a6d5abb7caf1b1
matthew.gould <matthew.gould@morneaushepell.com>
2023-02-04 06:58:43 +0000

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const nextJest = require('next/jest')
 
+const esModules = ['@codesandbox'].join('|')
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './'
@@ -12,8 +14,10 @@ const customJestConfig = {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/(.*)$': '<rootDir>/$1'
   },
-
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jest-environment-jsdom',
+  transformIgnorePatterns: [
+    `[/\\\\]node_modules[/\\\\](?!${esModules}).+\\.(js|jsx|mjs|cjs|ts|tsx)$`
+  ]
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
