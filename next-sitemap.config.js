816640ea2f4869f1b294891d5d0760f6fb43d2de
matthew.gould <matthew.gould@morneaushepell.com>
2023-02-10 17:59:23 +0000

    add sitemap & robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://matthewgould.dev',
+  generateIndexSitemap: false,
+  generateRobotsTxt: true // (optional)
+  // ...other options
+}
